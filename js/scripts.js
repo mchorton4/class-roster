36cfd7ecb1aa0a79ddd7a341616b80d6fa021306
EWU Web.Lab <pmanikoth@ewu.edu>
2022-04-20 12:14:12 -0700

    Boilerplate

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,30 @@
 // My Scripts
-console.log(‘OK’);
+
+
+// Data source
+const url = "https://picsum.photos/v2/list";
+
+// Get data
+fetch(url)
+  .then( response => response.json() )
+  .then( data  => {
+    // check-check
+    console.log(data);
+    
+    // Get Parent
+    const container = document.querySelector('main');
+     
+    // Loop through each item
+    data.forEach( ( item ) => {
+      
+     const template = `
+     <figure>
+        <img src=${item.src = item.download_url} />  
+        <figcaption>${item.author}</figcaption>
+     </figure>
+     `
+     // Dynamicaly insert into the DOM
+      container.insertAdjacentHTML('afterbegin', template);
+
+    });
+  });
